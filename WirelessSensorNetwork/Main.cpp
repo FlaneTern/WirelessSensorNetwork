9afa830bcde59a38024bafb3fdc21d931a9bf370
luciennezzz <hertgeathertmc@gmail.com>
2023-11-18 19:31:17 +0700

    A lot

--- WirelessSensorNetwork/Main.cpp
+++ WirelessSensorNetwork/Main.cpp
@@ -4,34 +4,41 @@
 #include "Simulation.h"
 
 
+static constexpr int s_TotalDurationToBeTransferred = 3600 * 24 * 90;
+static constexpr int s_TransferTime = 60;
+static constexpr int s_RecoveryTime = 30;
+
 int main()
 {
-	for (int redo = 2; redo < 3; redo++)
+
+	for (int redo = 100000; redo < 100001; redo++)
 	{
 		for (int meanMultiplier = 1; meanMultiplier <= 8; meanMultiplier *= 8)
 		{
 			for (int stddev = 900; stddev <= 7200; stddev += 900)
 			{
 				std::cout << "Starting :\t Redo : " << redo << ",\t Standard Deviation : " << stddev * meanMultiplier << ",\t Mean : " << 3600 * meanMultiplier << '\n';
-
-				// totalDuration, transferTime, recoveryTime, mean, stddev, redo
-				WSN::Simulation* Si = new WSN::Simulation(86400 * 90, 60, 30, 3600 * meanMultiplier, stddev * meanMultiplier, redo);
+				int currentMean = 3600 * meanMultiplier;
+				// totalDurationToBeTransferred, transferTime, recoveryTime, mean, stddev, redo
+				WSN::Simulation* Si = new WSN::Simulation(s_TotalDurationToBeTransferred, s_TransferTime, s_RecoveryTime, currentMean, stddev * meanMultiplier, redo);
 
 
 				Si->SimulateAll();
 
 				// start delta, end delta, delta step
-				Si->BruteForceAll(1, 100000, 1);
+				Si->BruteForceAll(1, 3 * Si->GetDeltaOpt(), 1);
 
 				Si->Summarize();
+				Si->LogCDF();
 
 				delete Si;
+				WSN::Simulation::LogSummary();
 			}
 		}
 	}
 
-	WSN::Simulation::LogSummary();
-
+	WSN::Simulation::AverageAllRedos(100000, 100000);
+	//throw std::runtime_error("DONE !");
 
 	return 0;
 }
